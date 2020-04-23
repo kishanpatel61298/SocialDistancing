d4c36965b88b970668486c3e21fd3faf021f282a
Kishan Patel <32492255+kishanpatel61298@users.noreply.github.com>
2020-04-23 15:08:36 +0530

    Update SocialDistancing.py

--- SocialDistancing.py
+++ SocialDistancing.py
@@ -1,5 +1,5 @@
 # To read and write back out to video:
-# python SocialDistancing.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 --output output/output_01.avi
+# python SocialDistancing.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input example_01.mp4 --output output/output_01.avi
 #
 # To read from webcam and write back out to disk:
 # python SocialDistancing.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --output output/webcam_output.avi
